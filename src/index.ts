77b8e0ed04a7e6352a2c41edcf50be035d1d9bd2
Encoded <75113441+Encoded404@users.noreply.github.com>
2026-05-05 10:28:07 +0200

    update logging

--- src/index.ts
+++ src/index.ts
@@ -268,7 +268,7 @@ function getWorldInfo(): { charLore?: CharLoreSetting[] } | undefined {
 		}
         else
         {
-            if(sillyTavern.world_info)
+            if(sillyTavern.world_info && typeof sillyTavern.world_info === 'object' && !Array.isArray(sillyTavern.world_info))
             {
                 logWarn('getWorldInfo: SillyTavern global found but world_info is missing charLore or is an unexpected type. here is world_info:', sillyTavern.world_info);
             }
